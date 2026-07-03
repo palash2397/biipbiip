0bdab30390a0d94ec5e11dd99f939b6e9f4a5094
palash2397 <palashpatil55@gmail.com>
2026-07-03 19:18:43 +0530

    chore: configure foundational boilerplate
    - Add Swagger documentation setup
    - Add Morgan logging and global validation pipes
    - Set global API prefix and constants
    - Clean up default app service and test files
    - Add .env.example

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export default {
+  SWAGGER: {
+    TITLE: 'Biip Biip Exchange API',
+    DESCRIPTION: 'API documentation for Biip Biip Exchange System',
+    VERSION: '1.0',
+    SERVER_URL: '',
+  },
+
+  Global: {
+    PREFIX: '/api/v1',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,79 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+
+import morgan from 'morgan';
+
+import constants from './constants';
+const { SWAGGER, Global } = constants;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // morgan for logging
+  app.use(morgan('dev'));
+
+  // Serve uploaded files statically
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
+  // enable global validation for DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  //  cors
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // // WebSocket Adapter
+  // app.useWebSocketAdapter(new IoAdapter(app));
+
+  // Global Prefix
+  app.setGlobalPrefix(Global.PREFIX);
+
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle(SWAGGER.TITLE)
+    .setDescription(SWAGGER.DESCRIPTION)
+    .setVersion(SWAGGER.VERSION)
+    // .addServer(SWAGGER.SERVER_URL)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(`${Global.PREFIX}/docs`, app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.enableShutdownHooks();
+  await app.listen(process.env.PORT ?? 4010);
+  console.log(`🚀 Biip Biip server is running on port ${process.env.PORT}`);
 }
 bootstrap();
