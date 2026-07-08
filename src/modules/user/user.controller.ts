8b73f671acdcf8f7b4277fe75c19d869bd460170
palash2397 <palashpatil55@gmail.com>
2026-07-08 15:22:46 +0530

    feat(user): implement myProfile service and secure user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+import { RoleGuard } from '../auth/roles/roles.guard';
+import { Roles } from 'src/modules/auth/roles/roles.decorator';
+
 import { UserService } from './user.service';
 
+@ApiTags('User')
+@ApiBearerAuth('access-token')
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 import { ApiResponse } from 'src/helpers/ApiResponse';
 import { generateOtp, getExpirationTime } from 'src/helpers/index';
 
+import { Msg } from 'src/helpers/responseMsg';
+
 import { User, UserDocument } from './schema/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async myProfile(userId: string) {
+    try {
+      const user = await this.userModel
+        .findById(userId)
+        .select('-otp -otpExpireAt');
+
+      if (!user) {
+        return new ApiResponse(400, {}, Msg.USER_NOT_FOUND);
+      }
+
+      return new ApiResponse(200, user, Msg.USER_FETCHED);
+    } catch (error) {
+      console.log('error while getting my profile', error);
+
+      return new ApiResponse(500, {}, Msg.SERVER_ERROR);
+    }
+  }
+}
