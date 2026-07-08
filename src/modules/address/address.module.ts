948d6428ea4e070a1d60ef9b5dee5d67cb1a0669
palash2397 <palashpatil55@gmail.com>
2026-07-08 19:07:03 +0530

    feat(address): integrate mongoose schemas into address module

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AddressService } from './address.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { Address, AddressSchema } from './schema/address.schema';
+import { User, UserSchema } from '../user/schema/user.schema';
+
 import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Address.name, schema: AddressSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [AddressController],
   providers: [AddressService],
+
+  exports: [
+    MongooseModule.forFeature([{ name: Address.name, schema: AddressSchema }]),
+  ],
 })
 export class AddressModule {}
