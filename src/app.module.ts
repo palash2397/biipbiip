7487590b6ad4d004c199ecd4efb52653f4282a0c
palash2397 <palashpatil55@gmail.com>
2026-07-08 11:17:14 +0530

    feat(app): integrate configuration and database connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/databese.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
