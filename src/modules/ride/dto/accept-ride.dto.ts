d12f4f0132c21a9c911c1f77c7ad0aa49353a8ad
palash2397 <palashpatil55@gmail.com>
2026-07-16 14:39:15 +0530

    swagger configuration in accept ride dto

--- src/modules/ride/dto/accept-ride.dto.ts
+++ src/modules/ride/dto/accept-ride.dto.ts
@@ -1,6 +1,9 @@
-import { IsMongoId } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsNotEmpty } from 'class-validator';
 
 export class AcceptRideDto {
+  @ApiProperty()
+  @IsNotEmpty()
   @IsMongoId()
   rideId: string;
 }
