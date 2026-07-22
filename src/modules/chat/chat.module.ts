1b9b785a9deb91a88d5d921c573ec0a66655c917
palash2397 <palashpatil55@gmail.com>
2026-07-22 14:25:12 +0530

    chat module

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -2,8 +2,25 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatMessage, ChatMessageSchema } from './schema/chat-message.schema';
+import { User, UserSchema } from 'src/modules/user/schema/user.schema';
+import { Ride, RideSchema } from '../ride/schema/ride.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ChatMessage.name, schema: ChatMessageSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Ride.name, schema: RideSchema },
+    ]),
+  ],
   controllers: [ChatController],
   providers: [ChatService],
+  exports: [
+    MongooseModule.forFeature([
+      { name: ChatMessage.name, schema: ChatMessageSchema },
+    ]),
+  ],
 })
 export class ChatModule {}
