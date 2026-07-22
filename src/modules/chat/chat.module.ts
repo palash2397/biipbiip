96a8956c51c6ea0068e2e3a9c8d3ff707c26edfd
palash2397 <palashpatil55@gmail.com>
2026-07-22 14:28:46 +0530

    update chat module

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+// import { ChatGateway } from './chat.gateway';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatMessage, ChatMessageSchema } from './schema/chat-message.schema';
 import { User, UserSchema } from 'src/modules/user/schema/user.schema';
-import { Ride, RideSchema } from '../ride/schema/ride.schema';
+import { Ride, RideSchema } from 'src/modules/ride/schema/ride.schema';
+import { Driver, DriverSchema } from 'src/modules/driver/schema/driver.schema';
+
+import { SocketModule } from 'src/modules/socket/socket.module';
 
 @Module({
   imports: [
+    SocketModule,
     MongooseModule.forFeature([
       { name: ChatMessage.name, schema: ChatMessageSchema },
       { name: User.name, schema: UserSchema },
       { name: Ride.name, schema: RideSchema },
+      { name: Driver.name, schema: DriverSchema },
     ]),
   ],
   controllers: [ChatController],
