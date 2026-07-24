b62adad19ce8e67017b9c18ef6a29019ffacac0b
palash2397 <palashpatil55@gmail.com>
2026-07-24 11:27:16 +0530

    rating controller

--- src/modules/rating/rating.controller.ts
+++ src/modules/rating/rating.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { RatingService } from './rating.service';
 
+import { ApiBearerAuth, ApiTags, ApiConsumes } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+
+import { RoleGuard } from '../auth/roles/roles.guard';
+import { Roles } from '../auth/roles/roles.decorator';
+import { UserRole } from 'src/common/enums/user/role.enum';
+
+import { CreateRatingDto } from './dto/create-rating.dto';
+
+@ApiTags('Rating')
+@ApiBearerAuth('access-token')
+@UseGuards(JwtAuthGuard)
 @Controller('rating')
 export class RatingController {
   constructor(private readonly ratingService: RatingService) {}
+
+  @Post('/submit')
+  @Roles(UserRole.USER, UserRole.DRIVER)
+  @UseGuards(RoleGuard)
+  submitRating(@Req() req: any, @Body() dto: CreateRatingDto) {
+    return this.ratingService.createRating(req.user.id, dto);
+  }
 }
