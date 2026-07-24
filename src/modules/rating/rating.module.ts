4dd95bcea5d1df0bf9b936572ff27f78f9bd520c
palash2397 <palashpatil55@gmail.com>
2026-07-24 11:07:52 +0530

    Updated rating module

--- src/modules/rating/rating.module.ts
+++ src/modules/rating/rating.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { RatingService } from './rating.service';
 import { RatingController } from './rating.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Rating, RatingSchema } from './schema/rating.schema';
+import { Ride, RideSchema } from '../ride/schema/ride.schema';
+import { User, UserSchema } from '../user/schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Rating.name, schema: RatingSchema },
+      { name: Ride.name, schema: RideSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [RatingController],
   providers: [RatingService],
+
+  exports: [
+    RatingService,
+    MongooseModule.forFeature([{ name: Rating.name, schema: RatingSchema }]),
+  ],
 })
 export class RatingModule {}
